f8b7613314d8121f5838071a98243c075f7cda65
132yse <1533540012@qq.com>
2018-02-28 17:07:23 +0800

    添加关闭功能

---
+++ component/close.js
@@ -0,0 +1,9 @@
+export default ()=>{
+	const HTML=	`
+		<div class="call">召唤春菜</div>
+	`
+	document.querySelector('.close').onclick=()=>{
+		document.querySelector('.ukagaka').innerHTML=''
+	}
+	
+}
--- component/main.js
+++ component/main.js
@@ -1,7 +1,7 @@
 import classes from '../static/style.css'
 import data from '../static/data.json'
 import drag from './drag'
-
+import close from './close'
 
 export default ()=>{
 		const max = data.data.length-1
@@ -9,8 +9,15 @@ export default ()=>{
 		let HTML=
 		`<div class="tips">${data.data[index].tip}</div>
 				<i class="icon"></i>
-				<div class="face ${data.data[index].face}"></div>`		
-		document.querySelector('.ukagaka').innerHTML=HTML	
+				<div class="face ${data.data[index].face}"></div>
+				<i class="close">×</i>`
 
+
+		document.querySelector('.ukagaka').innerHTML=HTML
 		drag()
+		close()
+
+		
+
 }
+	
